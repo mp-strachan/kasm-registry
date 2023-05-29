c6a59847769840a6b92005939be6c1eeec698774
mp-strachan <26296076+mp-strachan@users.noreply.github.com>
2023-05-29 13:44:39 +1000

    Update next.config.js
    Matt S's updates

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Matt S',
+    description: 'Matt S Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/mp-strachan/kasm-registry/',
+    contactUrl: 'https://github.com/mp-strachan',
   },
   reactStrictMode: true,
   swcMinify: true,
